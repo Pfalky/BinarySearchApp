a3db05df6a011296ba362e3c7567cf715178894d
Pfalky <pfalky@yandex.ru>
2016-02-24 22:32:07 +0300

    Initial commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Root on 24.02.2016.
+ */
+public class BinarySearch {
+
+}
---
+++ src/BinarySearchApp.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Root on 24.02.2016.
+ */
+public class BinarySearchApp {
+    public static void main(String[] args) {
+
+    }
+
+}
