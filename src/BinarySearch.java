5fcce97a4e5517495e27c83878c2d91bc9b290a1
Pfalky <pfalky@yandex.ru>
2016-02-24 23:09:10 +0300

    Added Mylist class added returnMyList method

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+
 /**
  * Created by Root on 24.02.2016.
  */
 public class BinarySearch {
 
+    public BinarySearch() {
+    }
+
+    public int binarysearch(ArrayList){
+        int searchpos;
+
+        return 0;
+    }
 }
